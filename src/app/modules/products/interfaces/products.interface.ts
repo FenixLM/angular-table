22a61ddd6a9fbb41d32afaf272d834cbeba4acd7
Felix Lamadrid <felix.lamadrid97@gmail.com>
2024-04-07 03:55:01 -0500

    chore(products.interface.ts): add imageFile property to ProductInterface to store image file in request

--- src/app/modules/products/interfaces/products.interface.ts
+++ src/app/modules/products/interfaces/products.interface.ts
@@ -1,9 +1,12 @@
 export interface ProductInterface {
-    nombre?: string;
-    descripcion?: string;
-    sku?: string;
-    imagen?: string;
-    etiquetas?: string[];
-    precio?: number;
-    stock?: number;
+  nombre?: string;
+  descripcion?: string;
+  sku?: string;
+  imagen?: string;
+  etiquetas?: string[];
+  precio?: number;
+  stock?: number;
+
+  // Propiedad para almacenar el archivo de imagen en la solicitud
+  imageFile?: any;
 }
